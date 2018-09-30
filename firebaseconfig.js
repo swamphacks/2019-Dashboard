07400dcfe217bbca1dc9ffe4e1ee167026c72034
Debrian <debrianfigueroa@gmail.com>
2018-09-29 20:38:32 -0400

    uploaded necessary files

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAPcSN9HohHuNUKOTVJ0Fm3wfoAqUaxzhk",
+    authDomain: "swamphacks-2019.firebaseapp.com",
+    databaseURL: "https://swamphacks-2019.firebaseio.com",
+    projectId: "swamphacks-2019",
+    storageBucket: "swamphacks-2019.appspot.com",
+    messagingSenderId: "243219143648"
+};
+firebase.initializeApp(config);
