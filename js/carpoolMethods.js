05219d8b1053424c9ed2585ad5b0e4fedb2526fa
Debrian <debrianfigueroa@gmail.com>
2018-11-16 19:21:35 -0500

    switching branches

--- js/carpoolMethods.js
+++ js/carpoolMethods.js
@@ -1,33 +1,88 @@
 const driverRef = ref.child('carpoolDrivers');
 const passengerRef = ref.child('carpoolPassengers');
 
-function saveDriverInfo() {
-
+function saveDriverInfo(driverInfo) {
+    let data = {};
+    data[loggedInUserKey] = driverInfo;
+    driverRef.update(data);
 }
 
 function addPassenger() {
-    let data = {loggedInUserKey: true};
+    let data = {};
+    data[loggedInUserKey] = {
+        email: userObj.email,
+        fname: userObj.fname,
+        lname: userObj.lname,
+        transportationState: userObj.transportationState,
+        phoneNumber: userObj.phoneNumber
+    };
     passengerRef.set(data);
 }
 
 function addDriver() {
-    
+    let data = {};
+    data[loggedInUserKey] = true;
+    driverRef.set(data);
 }
 
-function acceptPassenger() {
+function acceptPassenger(userKey) {
     
 }
-
-function getDriverInfo() {
-    
+// How to use Promise: when you call the function, in order to get the value from it
+// it must be done like this:
+// isDriver(userKey).then(function(value) { value is = to true or false in this case
+//     // code goes here
+// });
+function isDriver(userKey) {
+    let driver = new Promise(function(resolve, reject) {
+        driverRef.child(userKey).once('value').then(function(snapshot) {
+            if (snapshot.val()) {
+                // is driver
+                resolve(true);
+            } else {
+                // is not driver
+                resolve(false);
+            }
+        });
+    });
+    return driver;
 }
-
-function getPassengerInfo() {
-    
+// How to use Promise: when you call the function, in order to get the value from it
+// it must be done like this:
+// isPassenger(userKey).then(function(value) {
+//     // code goes here
+// });
+function isPassenger(userKey) {
+    let passenger = new Promise(function(resolve, reject) {
+        passengerRef.child(userKey).once('value').then(function(snapshot) {
+            if (snapshot.val()) {
+                // is passenger
+                resolve(true);
+            } else {
+                // is not passenger
+                resolve(false);
+            }
+        });
+    });
+    return passenger;
+}
+// How to use: getDriverInfo(userKey).then(function(snapshot) {
+//      let driverInfo = snapshot.val();
+//      ...
+// });
+function getDriverInfo(userKey) {
+    return driverRef.child(userKey).once('value');
+}
+// How to use: getPassengerInfo(userKey).then(function(snapshot) {
+//      let passengerInfo = snapshot.val();
+//      ...
+// });
+function getPassengerInfo(userKey) {
+    return passengerRef.child(userKey).once('value');
 }
 
 function getDrivers() {
-    
+    // driverRef.
 }
 
 function getPassengers() {
