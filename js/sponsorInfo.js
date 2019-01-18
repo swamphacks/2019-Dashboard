ca3cb9b8123046bf6fd89b53164a172cdbbd12fe
Debrian <debrianfigueroa@gmail.com>
2019-01-18 15:33:19 -0500

    added sponsor data

---
+++ js/sponsorInfo.js
@@ -0,0 +1,283 @@
+const realtruckSponsor = [
+    {
+        name: 'Sid Millspaugh',
+        skills: 'PHP, iOS Objective-C, Node.js/JS, AWS, Software Architecture',
+        imageURL: ''
+    },
+    {
+        name: 'Henning Glatter-Götz',
+        skills: 'PHP, JS, Software Architecture',
+        imageURL: ''
+    },
+    {
+        name: 'Josh Deltener',
+        skills: 'Vue.js, JS, CSS, PHP, Algolia, ElasticSearch, MSSQL, Software Architecture',
+        imageURL: ''
+    },
+    {
+        name: 'Jake Carnevale',
+        skills: 'Vue.js, JS, Webpack, Pug, CSS/Stylus/Sass/Less, Algolia',
+        imageURL: ''
+    },
+    {
+        name: 'Christopher Flint',
+        skills: 'NodeJS, Java, Microservices, Serverless, AWS, Internet scale distributed web applications, TDD, CI/CD, DynamoDB, MongoDB, SQL',
+        imageURL: ''
+    },
+    {
+        name: 'Eric Shinn',
+        skills: 'JavaScript, NodeJS, Webpack, Pug, CSS/Stylus/Sass/Less, React, Jest/Jasmine, AWS Lambda/Serverless, CircleCI, Git/Gitlab/Github/GH-Pages, Vim, NextJS, Heroku, JamStack, PHP, MySQL',
+        imageURL: ''
+    },
+    {
+        name: 'Wes Sadler',
+        skills: 'PHP, JavaScript, ASP, JSP, CSS, C#, Oracle SQL, MySQL, Software Architecture, Certified Agile Developer, Advanced ScrumMaster',
+        imageURL: ''
+    },
+    {
+        name: 'Prarabdh Joshi',
+        skills: 'JavaScript, Node.js, TDD, AWS, Serverless, Microservices, DynamoDB',
+        imageURL: ''
+    },
+];
+
+const citiSponsor = [
+    {
+        name: 'Ana Jelacic',
+        skills: 'Web Development (Angular 5/HTML/CSS/ JS), UI/ UX Design, Git, Technical Writing, Public Speaking',
+        imageURL: ''
+    },
+    {
+        name: 'Adam Wexler',
+        skills: 'Core Java Developer (Spring), Software Design, and other web frameworks',
+        imageURL: ''
+    },
+    {
+        name: 'Brandon Goldman',
+        skills: 'Web Dev (MEAN/MERN, HTML/CSS/JS), UI/UX Design, Git',
+        imageURL: ''
+    },
+    {
+        name: 'Jacob Williamson',
+        skills: 'Developer Evangelist, iOS Dev, Embedded Systems, Microcontrollers, and UI/UX Design',
+        imageURL: ''
+    },
+    {
+        name: 'Mike McCall',
+        skills: 'Talk to me for information about getting hired by Citi, or for advice on interviewing with other companies',
+        imageURL: ''
+    },
+];
+
+const amexSponsor = [
+    {
+        name: 'Stephanie Cruz',
+        skills: '',
+        imageURL: ''
+    },
+    {
+        name: 'Adrian Moya',
+        skills: 'front-end (React), back-end (Java, Node, .Net), databases (SQL & noSQL), Googling just the right phrase, opening 3 copies of the same tab on Chrome',
+        imageURL: ''
+    },
+    {
+        name: 'Kyle McKissack',
+        skills: 'MEAN stack, NoSQL DBs, Java',
+        imageURL: ''
+    },
+];
+
+const infiniteSponsor = [
+    {
+        name: 'Alex Whitler',
+        skills: 'Java, C#, Go, Kotlin, Java EE, .NET, .NET Core, Cordova (hybrid apps), Maven, Gradle, Some webpack, RESTful services, Docker/kubernetes, Android (though it\'s been a while)',
+        imageURL: ''
+    },
+    {
+        name: 'Heather Fullen',
+        skills: 'SQL, r, Data Science, specifically Machine Learning and Predictive Analytics',
+        imageURL: ''
+    },
+    {
+        name: 'Ryan Davis',
+        skills: 'C#, javascript, python, bash, SQL, common lisp, java',
+        imageURL: ''
+    },
+    {
+        name: 'Phillip Smith',
+        skills: 'Java, C++, C#, Javascript, Some Python/C, AngularJS, Angular, React',
+        imageURL: ''
+    },
+    {
+        name: 'Neha Singh',
+        skills: 'C#, Angular, React, Java, SQL',
+        imageURL: ''
+    },
+    {
+        name: 'Bo Dong',
+        skills: 'Java, Angular, C#, SQL',
+        imageURL: ''
+    },
+    {
+        name: 'Justin T. Bricker',
+        skills: 'C#, Java, Python, SQL, JavaScript, React',
+        imageURL: ''
+    },
+    {
+        name: 'Kedar Thakur',
+        skills: 'Java, C#, HTML, CSS, JavaScript, SQL',
+        imageURL: ''
+    },
+    {
+        name: 'Joshua Novick',
+        skills: 'C# (.NET Core and .NET Framework), Docker, pipelines, React, SQL',
+        imageURL: ''
+    },
+    {
+        name: 'Sylvia Lujo',
+        skills: 'MEAN stack, React w/ Firebase, python, raspberry pi, C++, Java, JavaScript',
+        imageURL: ''
+    },
+    {
+        name: 'Aparna Venkatesan',
+        skills: 'C# (.NET Core and .NET Framework), Docker, SQL, RabbitMQ',
+        imageURL: ''
+    },
+];
+
+const jpSponsor = [
+    {
+        name: 'Clayton Schnelker',
+        skills: 'System Design, Java',
+        imageURL: ''
+    },
+    {
+        name: 'Natalie Veilleux',
+        skills: 'Java/Javascript',
+        imageURL: ''
+    },
+    {
+        name: 'Alex Clark',
+        skills: 'Java/Javascript, Spring microservices, Reactjs',
+        imageURL: ''
+    },
+    {
+        name: 'Vinnie Plantamura',
+        skills: 'Technology Campus Recruiting',
+        imageURL: ''
+    },
+];
+
+const infoTechSponsor = [
+    {
+        name: 'Shafiq Mohammed',
+        skills: 'ITI Products',
+        imageURL: ''
+    },
+    {
+        name: 'Amit Agarwal',
+        skills: 'AASHTOWare',
+        imageURL: ''
+    },
+    {
+        name: 'Stefano Reyna',
+        skills: 'AASHTOWare',
+        imageURL: ''
+    },
+    {
+        name: 'Christopher Clarke',
+        skills: 'ITI Products',
+        imageURL: ''
+    },
+    {
+        name: 'Daniella Rossi',
+        skills: '',
+        imageURL: ''
+    },
+    {
+        name: 'Lindsey Day',
+        skills: '',
+        imageURL: ''
+    },
+];
+
+const homeDepotSponsor = [
+    {
+        name: 'Sanjay Nair',
+        skills: 'Java, Node, React, Google Cloud Platform',
+        imageURL: ''
+    },
+    {
+        name: 'Edwin Villatoro',
+        skills: 'Java (Android, Springboot), Swift (iOS), React (Web)',
+        imageURL: ''
+    },
+    {
+        name: 'Brian Lam',
+        skills: 'Java, Kotlin, JavaScript, Ruby, Google Cloud Platform',
+        imageURL: ''
+    },
+    {
+        name: 'Daniel Salmeron',
+        skills: 'HTML, CSS, JavaScript, NodeJS, ReactJS',
+        imageURL: ''
+    },
+];
+
+const microsoftSponsor = [
+    {
+        name: 'Killian McCoy',
+        skills: 'UWP, C#, Windows ML, re-learning web dev with emphasis on PWAs ',
+        imageURL: ''
+    },
+    {
+        name: 'Brandon Smock',
+        skills: 'Python, Java, machine learning, deep learning',
+        imageURL: ''
+    },
+];
+
+const googleSponsor = [
+    {
+        name: 'Harsha Ram',
+        skills: 'I can help with some parts of Google Cloud Platform like Firebase, App engine and the machine learning algorithms like speech, vision, translation.',
+        imageURL: ''
+    },
+    {
+        name: 'Sandeep Gommaraju',
+        skills: '',
+        imageURL: ''
+    },
+];
+
+const facebookSponsor = [
+    {
+        name: '',
+        skills: '',
+        imageURL: ''
+    },
+];
+
+const linodeSponsor = [
+    {
+        name: '',
+        skills: '',
+        imageURL: ''
+    },
+];
+
+const ultimateSponsor = [
+    {
+        name: '',
+        skills: '',
+        imageURL: ''
+    },
+];
+
+const exxonSponsor = [
+    {
+        name: '',
+        skills: '',
+        imageURL: ''
+    },
+];
